a95f6fae3407b1c5d8ab3fa686d60cee8237106b
Pavel Sedoy <pavliksedoy@gmail.com>
2019-08-26 00:27:48 +0300

    Title and meta description from React App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+// Import Libs
+import { Helmet } from 'react-helmet'
+// Style
 import './App.scss'
 
 function App() {
   return (
-    <div>s</div>
+    <>
+      <Helmet>
+        <title>Title from Helmet Test</title>
+        <meta name="description" content="Descriprion from Helmet" />
+      </Helmet>
+      s
+    </>
   );
 }
 
